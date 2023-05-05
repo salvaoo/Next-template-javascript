bbe113a9cee0c8c485036fc7452d801fae0be512
salvaoo <salvadorgilrosales@outlook.es>
2023-05-05 11:48:16 +0100

    Added the shadcn template and shadcn/ui

---
+++ src/config/site.js
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "A Next.js template with shadcn/ui components. This template is a JavaScript version.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/salvagr_",
+    shadcnGithub: "https://github.com/shadcn/ui",
+    github: "https://github.com/salvaoo",
+    docs: "https://ui.shadcn.com",
+  },
+}
